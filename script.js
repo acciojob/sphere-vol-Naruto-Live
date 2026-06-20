f7acfd981b0f057562e991b1096c96d7fcf09b09
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-21 00:03:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function volume_sphere() {
-    //Write your code here
+    function volume_sphere() {
+	let res = document.getElementById('volume')
+	let r = Number(document.getElementById('radius').value)
+	
+    if(isNaN(r) || r < 0) {
+		res.value = "NaN"
+	}
+	
+	let volume = ( 4 / 3 ) * Math.PI * r * r * r ;
+	res.value = volume.toFixed(4);
+  
+} 
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
   
 } 
 
